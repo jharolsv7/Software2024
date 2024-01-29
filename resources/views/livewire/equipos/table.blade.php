5ccf730b24566218c6767169164ed78e3b0075ce
Diego-Hidalgo <dshidalg@espe.edu.ec>
2024-01-29 07:02:26 -0500

    modificacion home

--- resources/views/livewire/equipos/table.blade.php
+++ resources/views/livewire/equipos/table.blade.php
@@ -1,27 +1,32 @@
-<table class="w-full border-collapse border mt-2 text-center rounded-lg overflow-hidden shadow-lg">
-<!-- Encabezados de la tabla -->
-    <thead class="bg-gray-700 text-white">
-        <tr>
-            <th class="border px-4 py-2">Equipo</th>
-            <th class="border px-4 py-2">Goles a Favor</th>
-            <th class="border px-4 py-2">Goles en Contra</th>
-            <th class="border px-4 py-2">Puntos</th>
-        </tr>
-    </thead>
+<div class="overflow-x-auto">
+    <table class="w-full border-collapse border mt-4 text-center rounded-lg overflow-hidden shadow-lg text-sm md:text-base">
+        <!-- Encabezados de la tabla -->
+        <thead class="bg-gradient-to-r from-blue-500 to-blue-800 text-white">
+            <tr>
+                <th class="border px-2 py-1 md:px-4 md:py-2">Equipo</th>
+                <th class="border px-2 py-1 md:px-4 md:py-2">Goles a Favor</th>
+                <th class="border px-2 py-1 md:px-4 md:py-2">Goles en Contra</th>
+                <th class="border px-2 py-1 md:px-4 md:py-2">Puntos</th>
+            </tr>
+        </thead>
 
-    <!-- Contenido de la tabla -->
-    <tbody class="bg-gray-500">
-        @forelse($equipos as $row)
-            <tr>
-                <td>{{ $row->nombre }}</td>
-                <td>{{ $row->goles_a_favor }}</td>
-                <td>{{ $row->goles_en_contra }}</td>
-                <td>{{ $row->puntos }}</td>
-            </tr>
-        @empty
-            <tr>
-                <td class="text-center" colspan="100%">Sin Datos</td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+        <!-- Contenido de la tabla -->
+        <tbody class="bg-gray-700">
+            @forelse($equipos as $row)
+                <tr class="hover:bg-gray-800">
+                    <td class="border px-2 py-1 md:px-4 md:py-2 text-white bg-blue-600">
+                        {{ $row->nombre }}
+                    </td>
+                    <td class="border px-2 py-1 md:px-4 md:py-2 bg-blue-200">{{ $row->goles_a_favor }}</td>
+                    <td class="border px-2 py-1 md:px-4 md:py-2 bg-blue-300">{{ $row->goles_en_contra }}</td>
+                    <td class="border px-2 py-1 md:px-4 md:py-2 bg-blue-400 text-white">{{ $row->puntos }}</td>
+                </tr>
+            @empty
+            <tr class="hover:bg-gray-800 transition-bg duration-300">
+
+                    <td colspan="4" class="border px-2 py-1 md:px-4 md:py-2 bg-gray-300 text-black">Sin Datos</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+</div>
--- resources/views/livewire/partidos/table.blade.php
+++ resources/views/livewire/partidos/table.blade.php
@@ -1,30 +1,38 @@
-<table class="w-full border-collapse border mt-2 text-center rounded-lg overflow-hidden shadow-lg">
-    <!-- Encabezados de la tabla -->
-    <thead class="bg-gray-700 text-white">
-        <tr>
-            <th class="border px-4 py-2">Equipo Local</th>
-            <th class="border px-4 py-2">Equipo Visitante</th>
-            <th class="border px-4 py-2">Ubicación</th>
-            <th class="border px-4 py-2">Fecha</th>
-            <th class="border px-4 py-2">Hora</th>
-        </tr>
-    </thead>
+<div class="overflow-x-auto">
+    <div class="table-responsive">
+        <table class="w-full md:table-auto border-collapse border mt-2 text-center rounded-lg overflow-hidden shadow-lg">
+            <!-- Encabezados de la tabla -->
+            <thead class="bg-gradient-to-r from-blue-500 to-blue-800 text-white">
+                <tr>
+                    <th class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3">Equipo Local</th>
+                    <th class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3">Equipo Visitante</th>
+                    <th class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3">Ubicación</th>
+                    <th class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3">Fecha</th>
+                    <th class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3">Hora</th>
+                </tr>
+            </thead>
 
-    <!-- Contenido de la tabla -->
-    <tbody class="bg-gray-500">
-        @forelse($partidos as $partido)
-            <tr>
-                <td>{{ optional($partido->equipo)->nombre }}</td>
-                <td>{{ optional($partido->equipoDos)->nombre }}</td>
-                <td>{{ $partido->ubicacion }}</td>
-                <td>{{ $partido->fecha }}</td>
-                <td>{{ $partido->hora }}</td>
-                <!-- Otros campos según tus necesidades -->
-            </tr>
-        @empty
-            <tr>
-                <td colspan="100%">Sin partidos encontrados</td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+            <!-- Contenido de la tabla -->
+            <tbody class="bg-gray-700">
+                @forelse($partidos as $partido)
+                    <tr class="hover:bg-gray-800">
+                        <td class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3 text-white bg-blue-600 text-xs md:text-sm lg:text-base">
+                            {{ optional($partido->equipo)->nombre }}
+                        </td>
+                        <td class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3 bg-blue-200 text-xs md:text-sm lg:text-base">
+                            {{ optional($partido->equipoDos)->nombre }}
+                        </td>
+                        <td class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3 bg-blue-300 text-xs md:text-sm lg:text-base">{{ $partido->ubicacion }}</td>
+                        <td class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3 bg-blue-400 text-xs md:text-sm lg:text-base">{{ $partido->fecha }}</td>
+                        <td class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3 bg-blue-500 text-xs md:text-sm lg:text-base">{{ $partido->hora }}</td>
+                        <!-- Otros campos según tus necesidades -->
+                    </tr>
+                @empty
+                    <tr class="hover:bg-gray-800">
+                        <td colspan="5" class="border px-2 py-1 md:px-4 md:py-2 lg:px-6 lg:py-3 bg-gray-300 text-black text-xs md:text-sm lg:text-base">Sin partidos encontrados</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</div>
