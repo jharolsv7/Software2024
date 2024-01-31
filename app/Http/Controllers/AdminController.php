0d1c93dc5b68e67a68ab2cf5abb40e836a84689d
Joel976 <ajcuenca4@espe.edu.ec>
2024-01-31 08:06:22 -0500

    Creación del Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,109 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
-    public function index(){
-        return view('admin.index');
+    public function index()
+    {
+        // Obtener datos de ingresos y egresos en una sola consulta
+        $data = DB::table('ingresos')
+                ->select('detalles', DB::raw('SUM(monto) as ingresos'), DB::raw('0 as egresos'))
+                ->groupBy('detalles')
+                ->unionAll(DB::table('egresos')
+                ->select('detalles', DB::raw('SUM(monto) as ingresos'), DB::raw('0 as egresos'))
+                ->groupBy('detalles'))
+                ->get();
+
+
+        // Organizar datos fusionados en un array de objetos
+        $mergedData = [];
+        foreach ($data as $item) {
+        $mergedData[$item->detalles] = (object) [
+            'detalles' => $item->detalles,
+            'ingresos' => $item->ingresos,
+            'egresos' => $item->egresos
+            ];
+        }
+
+        // Preparar datos para el gráfico combinado de ingresos y egresos
+        $labels = [];
+        $ingresosData = [];
+        $egresosData = [];
+
+        foreach ($mergedData as $data) {
+        $labels[] = $data->detalles;
+        $ingresosData[] = $data->ingresos;
+        $egresosData[] = $data->egresos;
+        }
+
+
+        // Obtener datos de la tabla 'goleadores'
+        $goleadores = DB::table('goleadores')
+                        ->select('jugador_id', 'goles') // Seleccionar tanto el jugador como el número de goles
+                        ->orderBy('goles', 'desc') // Ordenar por número de goles en orden descendente
+                        ->get();
+
+        // Preparar datos para el gráfico de goleadores
+        $goleadoresData = [];
+
+        foreach ($goleadores as $goleador) {
+            $goleadoresData[] = [
+                'jugador_id' => $goleador->jugador_id,
+                'goles' => $goleador->goles
+            ];
+        }
+        // Obtener datos de la tabla 'partidos'
+        $partidos = DB::table('partidos')
+        ->select('ganador', 'golesEquipo1', 'golesEquipo2')
+        ->get();
+
+        // Inicializar contadores
+        $ganados = 0;
+        $empatados = 0;
+        $perdidos = 0;
+
+        foreach ($partidos as $partido) {
+        // Comparar los puntajes para determinar el resultado del partido
+        if ($partido->ganador == 'equipo_uno') {
+            $ganados++;
+        } elseif ($partido->ganador == 'equipo_dos') {
+            $perdidos++;
+        } else {
+            if ($partido->golesEquipo1 == $partido->golesEquipo2) {
+                $empatados++;
+            } else {
+                // Suponiendo que si no hay ganador, hay un empate
+                $empatados++;
+            }
+        }
+        }
+
+// Crear el array con los datos de los partidos
+$partidosData = [
+'Ganados' => $ganados,
+'Empatados' => $empatados,
+'Perdidos' => $perdidos
+];
+
+
+    
+        // Obtener datos de la tabla 'tarjetas'
+        $tarjetas = DB::table('partidos')
+        ->select(DB::raw('SUM(tarjetaAmarilla) as amarillas'), DB::raw('SUM(tarjetaRoja) as rojas'))
+        ->get();
+
+        // Preparar datos para el gráfico de tarjetas
+        $tarjetasData = [];
+        foreach ($tarjetas as $tarjeta) {
+        $tarjetasData = [
+            'amarillas' => $tarjeta->amarillas,
+            'rojas' => $tarjeta->rojas
+        ];
+        }
+
+
+        return view('admin.index', compact('labels', 'ingresosData', 'egresosData', 'goleadoresData','partidosData','tarjetasData'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,160 @@
 @stop
 
 @section('content')
-    <h5 class="text-center">Administrador <b>{{Auth::user()->name}}</b> aquí puedes actulizar tus tablas del torneo</h5>
+    <h5 class="text-center">Administrador <b>{{Auth::user()->name}}</b> aquí puedes actualizar tus tablas del torneo</h5>
+    
+    <div class="row">
+        <div class="col-md-4">
+            <div>
+                <canvas id="myChartIngresosEgresos" width="400" height="400"></canvas>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div>
+                <canvas id="myChartGoleadores" width="400" height="400"></canvas>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div>
+                <canvas id="myChartPartidos" width="400" height="400"></canvas>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div>
+                <canvas id="myChartTarjetas" width="400" height="400"></canvas>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+        const ctxIngresosEgresos = document.getElementById('myChartIngresosEgresos');
+
+        new Chart(ctxIngresosEgresos, {
+            type: 'line',
+            data: {
+                labels: {!! json_encode($labels) !!},
+                datasets: [{
+                    label: 'Ingresos / Egresos',
+                    data: {!! json_encode($ingresosData) !!},
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 2
+                    
+                
+                },
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const ctxGoleadores = document.getElementById('myChartGoleadores');
+
+        new Chart(ctxGoleadores, {
+            type: 'doughnut',
+            data: {
+                labels: {!! json_encode(array_column($goleadoresData, 'jugador_id')) !!},
+                datasets: [{
+                    label: 'Goleadores',
+                    data: {!! json_encode(array_column($goleadoresData, 'goles')) !!},
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+
+        const ctxPartidos = document.getElementById('myChartPartidos');
+
+        new Chart(ctxPartidos, {
+            type: 'bar',
+            data: {
+                labels: ['Ganados', 'Empatados', 'Perdidos'],
+                datasets: [{
+                    label: 'Partidos',
+                    data: {!! json_encode(array_values($partidosData)) !!},
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+
+        const ctxTarjetas = document.getElementById('myChartTarjetas');
+
+        new Chart(ctxTarjetas, {
+            type: 'bar',
+            data: {
+                labels: ['Amarillas', 'Rojas'],
+                datasets: [{
+                    label: 'Tarjetas',
+                    data: {!! json_encode([$tarjetasData['amarillas'], $tarjetasData['rojas']]) !!},
+                    backgroundColor: [
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(255, 99, 132, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(255, 99, 132, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 @stop
 
 @section('css')
@@ -17,5 +170,3 @@
 @section('js')
     <script> console.log('Hi!'); </script>
 @stop
-
-
